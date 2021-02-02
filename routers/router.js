a8aa5d068295bde98e670906895f578462330919
gulbenemanet <75253931+gulbenemanet@users.noreply.github.com>
2021-02-02 22:06:35 +0300

    k

--- routers/router.js
+++ routers/router.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const anasayfa = require('../controller/anasayfa');
-// const sekiz = require('../controller/sekiz')
-// const onaltı = require('../controller/onaltı')
+const sekiz = require('../controller/sekiz')
+const onaltı = require('../controller/onaltı')
 
 const router = express.Router();
 
 router.get('/', anasayfa)
-    // router.get('/game8.html', sekiz)
-    // router.get('/game16.html', onaltı)
+router.get('/game8.html', sekiz)
+router.get('/game16.html', onaltı)
 
 module.exports = router;
